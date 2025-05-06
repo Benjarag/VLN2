93718afc5a3c14db12be3a9f931a396c0c1526a9
olliemil <oliverkjaran@gmail.com>
2025-05-06 13:40:30 +0000

    getting ever closer but need to ... see description
    Laga heart iconið að það verði bara rautt en ekki allur hringurinn, setja svo active virknina yfir í app.js og fá það til að virka og gera eitthvap check að hann verði að vera logged in. Og laga herbegi og stærð húsin takka location. Síðan byrja ég á að búa til filter search og alvöru logic.

--- data.py
+++ data.py
@@ -10,5 +10,31 @@ PROPERTIES = [
         "zipcode": "103",
         "image_url": "properties/images/Lautarvegur30.png"
 
+    },
+
+    {
+        "id": 2,
+        "title": "Djáknavegur 3, Selfoss, 806",
+        "size" : "126 sqm",
+        "price": "65.000.000 kr.",
+        "rooms": "5",
+        "status": "Available",
+        "type": "Sumarhús",
+        "zipcode": "806",
+        "image_url": "properties/images/Djaknavegur3.png"
+
+    },
+
+    {
+        "id": 3,
+        "title": "Álfkonuhvarf 33, Kópavogur, 203",
+        "size" : "131 sqm",
+        "price": "83.650.000 kr.",
+        "rooms": "4",
+        "status": "Available",
+        "type": "Fjölbýlishús",
+        "zipcode": "203",
+        "image_url": "properties/images/Alfkonuhvarf33.png"
+
     }
 ]
