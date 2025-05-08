fa13eb531f357fa43aa7f1ae2d9809c907ee1598
olliemil <oliverkjaran@gmail.com>
2025-05-08 14:59:28 +0000

    comment out

--- data.py
+++ data.py
@@ -1,90 +1,91 @@
-PROPERTIES = [
-    {
-        "id": 0,
-        "title": "Lautarvegur 30, Reykjavik, 103",
-        "size" : "310 sqm",
-        "price": "155.808.000 kr.",
-        "rooms": "7",
-        "status": "Available",
-        "type": "Einbýlishús",
-        "zipcode": "103",
-        "image_url": "properties/images/Lautarvegur30.png",
-        "data_listed": "2023-01-01",
-        "bathrooms": "1",
-        "description": "Built in 2020, feautures a large garden, modern interior and a large pool.",
-        "seller": {
-            "name": "John Doe",
-            "phone": "+354 555 1234",
-            "profile_image": "seller_images/john_doe.jpg"
-        }
-
-    },
-
-    {
-        "id": 1,
-        "title": "Djáknavegur 3, Selfoss, 806",
-        "size" : "126 sqm",
-        "price": "65.000.000 kr.",
-        "rooms": "5",
-        "status": "Sold",
-        "type": "Sumarhús",
-        "zipcode": "806",
-        "image_url":"properties/images/Djaknavegur3.png",
-        "data_listed": "2023-01-01",
-        "bathrooms": "1",
-        "description": "Built in 2020, feautures a large garden, modern interior and a large pool.",
-        "seller": {
-            "name": "Jane Smith",
-            "phone": "+354 555 5678",
-            "profile_image": "seller_images/jane_smith.jpg"
-        }
-
-    },
-
-    {
-        "id": 2,
-        "title": "Álfkonuhvarf 33, Kópavogur, 203",
-        "size" : "131 sqm",
-        "price": "83.650.000 kr.",
-        "rooms": "4",
-        "status": "Available",
-        "type": "Fjölbýlishús",
-        "zipcode": "203",
-        "image_url": "properties/images/Alfkonuhvarf33.png",
-        "data_listed": "2023-01-01",
-        "bathrooms": "1",
-        "description": "Built in 2020, feautures a large garden, modern interior and a large pool.",
-        "seller": {
-            "name": "Mike Johnson",
-            "phone": "+354 555 9012",
-            "profile_image": "seller_images/mike_johnson.jpg"
-        }
-
-    },
-
-    {
-        "id": 3,
-        "title": "Haukanes 24, Garðabær, 210",
-        "size": "620 sqm",
-        "price": "625.000.000 kr.",
-        "rooms": "20",
-        "bedrooms": "7",
-        "status": "Available",
-        "type": "Einbýlishús",
-        "zipcode": "210",
-        "image_url": "properties/images/Haukanes24.png",
-        "date": "2025-01-01",
-        "bathrooms": "4",
-        "description": "Húsið er á tveim hæðum. Gegið er inn á efri hæðina frá götu",
-        "seller": {
-            "name": "Styrmir Bjartur Karlson",
-            "phone": "899-9090",
-            "profile_image": "properties/images/Styrmir.png"
-        }
-
-
-    }
-
-
-
-]
+# PROPERTIES = [
+#     {
+#         "id": 0,
+#         "title": "Lautarvegur 30, Reykjavik, 103",
+#         "size" : "310 sqm",
+#         "price": "155.808.000 kr.",
+#         "rooms": "7",
+#         "status": "Available",
+#         "type": "Einbýlishús",
+#         "zipcode": "103",
+#         "image_url": "properties/images/Lautarvegur30.png",
+#         "data_listed": "2023-01-01",
+#         "bathrooms": "1",
+#         "description": "Built in 2020, feautures a large garden, modern interior and a large pool.",
+#         "seller": {
+#             "name": "John Doe",
+#             "phone": "+354 555 1234",
+#             "profile_image": "seller_images/john_doe.jpg"
+#         }
+#
+#     },
+#
+#     {
+#         "id": 1,
+#         "title": "Djáknavegur 3, Selfoss, 806",
+#         "size" : "126 sqm",
+#         "price": "65.000.000 kr.",
+#         "rooms": "5",
+#         "status": "Sold",
+#         "type": "Sumarhús",
+#         "zipcode": "806",
+#         "image_url":"properties/images/Djaknavegur3.png",
+#         "data_listed": "2023-01-01",
+#         "bathrooms": "1",
+#         "description": "Built in 2020, feautures a large garden, modern interior and a large pool.",
+#         "seller": {
+#             "name": "Jane Smith",
+#             "phone": "+354 555 5678",
+#             "profile_image": "seller_images/jane_smith.jpg"
+#         }
+#
+#     },
+#
+#     {
+#         "id": 2,
+#         "title": "Álfkonuhvarf 33, Kópavogur, 203",
+#         "size" : "131 sqm",
+#         "price": "83.650.000 kr.",
+#         "rooms": "4",
+#         "status": "Available",
+#         "type": "Fjölbýlishús",
+#         "zipcode": "203",
+#         "image_url": "properties/images/Alfkonuhvarf33.png",
+#         "data_listed": "2023-01-01",
+#         "bathrooms": "1",
+#         "description": "Built in 2020, feautures a large garden, modern interior and a large pool.",
+#         "seller": {
+#             "name": "Mike Johnson",
+#             "phone": "+354 555 9012",
+#             "profile_image": "seller_images/mike_johnson.jpg"
+#         }
+#
+#     },
+#
+#     {
+#         "id": 3,
+#         "title": "Haukanes 24, Garðabær, 210",
+#         "size": "620 sqm",
+#         "price": "625.000.000 kr.",
+#         "rooms": "20",
+#         "bedrooms": "7",
+#         "status": "Available",
+#         "type": "Einbýlishús",
+#         "zipcode": "210",
+#         "image_url": "properties/images/Haukanes24.png",
+#         "date": "2025-01-01",
+#         "bathrooms": "4",
+#         "description": "Húsið er á tveim hæðum. Gegið er inn á efri hæðina frá götu",
+#         "seller": {
+#             "name": "Styrmir Bjartur Karlson",
+#             "phone": "899-9090",
+#             "profile_image": "properties/images/Styrmir.png"
+#         }
+#
+#
+#     }
+#
+#
+#
+# ]
+#
