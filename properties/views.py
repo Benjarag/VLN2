073a99b3b5f59bf02d15b7d7bf2ab2829d8ab59f
Benjamín Ragnarsson <benjarag7@outlook.com>
2025-05-08 14:59:03 +0000

    push fix commented out

--- properties/views.py
+++ properties/views.py
@@ -1,15 +1,19 @@
 from django.shortcuts import render, get_object_or_404
-from data import PROPERTIES
+# from data import PROPERTIES  # This is commented out since we're using the model now
 from .models import Property
 
 def property_listings(request):
+    # Get all property objects from the database
+    properties = Property.objects.all()
+    
     return render(request, "properties/property_listings.html", {
-        "properties": PROPERTIES
+        "properties": properties
     })
 
-
-
 def property_details(request, property_id):
+    # Get a specific property or return a 404 if not found
+    property = get_object_or_404(Property, id=property_id)
+    
     return render(request, "properties/property_details.html", {
-        "property": PROPERTIES[property_id]
+        "property": property
     })
