fa9d925703d5b37732067508b0be73ae997c5e39
sindri-afk <sindribjarka@gmail.com>
2025-05-12 18:53:12 +0000

    is_seller

--- accounts/forms.py
+++ accounts/forms.py
@@ -12,6 +12,13 @@ class CustomUserCreationForm(UserCreationForm):
         required=True,
         widget=forms.TextInput(attrs={'class': 'form-control', 'id': 'signup_username', 'placeholder': 'Username'})
     )
+
+    usertype = forms.BooleanField(
+        required=False,
+        label='I am a seller',
+        help_text='Check this box if you are a seller'
+    )
+
     password1 = forms.CharField(
         required=True,
         widget=forms.PasswordInput(attrs={'class': 'form-control', 'id': 'signup_password1', 'placeholder': 'Password'})
@@ -29,8 +36,12 @@ class CustomUserCreationForm(UserCreationForm):
     def save(self, commit=True):
         user = super().save(commit=False)
         user.email = self.cleaned_data["email"]
+
         if commit:
             user.save()
+            profile, created = Profile.objects.get_or_create(user=user)
+            profile.is_seller = self.cleaned_data["usertype"]
+            profile.save()
         return user
 
 
--- accounts/models.py
+++ accounts/models.py
@@ -1,14 +1,16 @@
 from django.contrib.auth.models import User
 from django.db import models
+from django.template.context_processors import request
+
 from properties.models import Property
 
 
 # Create your models here.
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)  # here we are storing the user in the profile
-    image = models.ImageField(upload_to='profiles/', blank=True,
-                              null=True)  # here we are storing the image in the profiles folder
+    image = models.ImageField(upload_to='profiles/', blank=True, null=True)  # here we are storing the image in the profiles folder
     phone = models.CharField(max_length=20, blank=True)
+    is_seller = models.BooleanField(default=False)
 
     def __str__(self):
         return f'{self.user.username} Profile Page'
