a0440d5f226569d81cb7ba78dd3587e5fc790d68
olliemil <oliverkjaran@gmail.com>
2025-05-12 12:17:34 +0000

    functional home search bar added

--- home/views.py
+++ home/views.py
@@ -3,6 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 # home/views.py
 from django.http import HttpResponse
+from properties.forms import PropertyFilterForm
 
 def home_view(request):
-    return render(request, 'home/homepage.html')
+    # Create an empty search form instance
+    search_form = PropertyFilterForm()
+    return render(request, 'home/homepage.html', {'search_form': search_form})
