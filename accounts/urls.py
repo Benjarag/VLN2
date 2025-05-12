b9e7c312477c24d2766dea08eefb498a3d0f482f
sindri-afk <sindribjarka@gmail.com>
2025-05-12 16:43:57 +0000

    Style-aði login og signup
    bjó til login.css og signup.css, breytti báðum html og breytti forms.py

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,9 +2,10 @@
 from django.contrib.auth.views import LoginView
 from django.urls import path
 from . import views
+from .forms import CustomLoginForm
 
 urlpatterns = [
-    path('login/', LoginView.as_view(template_name='accounts/login.html'), name='login'),
+    path('login/', LoginView.as_view(template_name='accounts/login.html', authentication_form=CustomLoginForm), name='login'),
     path('signup/', views.signup_view, name='signup'),
     path('profile/', views.profile, name='profile'),
     path('profile/update/', views.profile_update, name='update_profile'),
