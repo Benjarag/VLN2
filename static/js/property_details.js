9acfb523f0901836a204f0abcd952c8479566ffd
olliemil <oliverkjaran@gmail.com>
2025-05-13 19:00:47 +0000

    near

--- static/js/property_details.js
+++ static/js/property_details.js
@@ -1,24 +1,25 @@
 document.addEventListener("DOMContentLoaded", function () {
+    const imagesContainer = document.querySelector(".carousel-images");
     const images = document.querySelectorAll(".carousel-image");
     const leftBtn = document.querySelector(".carousel-nav.left");
     const rightBtn = document.querySelector(".carousel-nav.right");
 
     let currentIndex = 0;
 
-    function showImage(index) {
-        images.forEach((img, i) => {
-            img.classList.remove("active");
-            if (i === index) img.classList.add("active");
-        });
+    function updateCarousel() {
+        const offset = -currentIndex * 100;
+        imagesContainer.style.transform = `translateX(${offset}%)`;
     }
 
     leftBtn.addEventListener("click", function () {
         currentIndex = (currentIndex - 1 + images.length) % images.length;
-        showImage(currentIndex);
+        updateCarousel();
     });
 
     rightBtn.addEventListener("click", function () {
         currentIndex = (currentIndex + 1) % images.length;
-        showImage(currentIndex);
+        updateCarousel();
     });
+
+    updateCarousel(); // Initial render
 });
